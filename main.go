9ab6152a037ecfb5aae6f16a0cf296f499199d9f
Mahatma <hatmahat@gmail.com>
2022-07-04 09:56:43 +0700

    main.go 1

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context)) {
-		c.JSON(http.Statuso)
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{
+	// 		"message": "pong",
+	// 	})
+	// })
+	// r.Run()
+
+	routerEngine := gin.Default()
+	routerEngine.GET("/", func(c *gin.Context) {
+		c.String(200, "Healthy Check")
+	})
+	err := routerEngine.Run(":8888")
+	if err != nil {
+		panic(err)
 	}
+
 }
